609a500dad5aad4f360dada8a9b11de05451c1e3
BGreenberg79 <greenbeb2@gmail.com>
2025-09-26 14:18:45 -0400

    1st config update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,8 @@ android {
 }
 
 dependencies {
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
@@ -51,4 +53,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    
 }
